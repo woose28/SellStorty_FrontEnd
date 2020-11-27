aec6258cc2edcfa5592692cfaac73a116bdfaf63
woose28 <dldntmdsla@naver.com>
2020-11-27 23:44:31 +0900

    Modify purchaseList.js

--- mypage/src/components/purchaseList.js
+++ mypage/src/components/purchaseList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LeftMenu from './leftMenu';
 import PurchaseItem from './purchaseItem';
+import Pagination from './pagination';
 import divider from '../img/divider.svg';
 
 const useStyles = makeStyles((theme) => ({
@@ -41,14 +42,53 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function PurchaseList(){
   const classes = useStyles();
+  const getInfo = () => {
+    let info = [];
+    for(let i = 0; i < 49; i++){
+      info[i] = {id:i, title:'책 이름 '+(i+1), author:'저자 이름 '+(i+1), price:String(i), download_date:'2020.01.01'}
+    }
+    return info
+  }  
+  
+  const [data] = React.useState({
+    info : getInfo(),
+    pageSize : 5
+  });
 
+  const [curPage, setCurPage] = React.useState(1)
+  const { length : count } = data.info;
+  const showingItem = data.info.slice(data.pageSize*(curPage-1), data.pageSize*curPage)
+  
+  const handlePageChange = (page) => {
+    setCurPage(page)
+  };
+
+  const itemAndPage = () => {
+    return (
+    <div>
+      {
+        showingItem.map(i => {
+          return <PurchaseItem title={i.title} author={i.author} price={i.price} download_date={i.download_date}/>    
+        })  
+      }
+      <Pagination cntItem={count} pageSize={data.pageSize} curPage={curPage} onChangePage={handlePageChange}/>
+    </div>)
+    }
+
+  const itemIsNone = () => {
+    return(
+      <div>데이터가 없습니다.</div>
+    )
+  }
   return(
     <div className={classes.root}>
       <LeftMenu mainGroup='panel2' subGroup='purchaseList'/>
       <div className={classes.content}>
         <div className={classes.txt_purchaseList}>구매한 전자책</div>
         <img src={divider} className={classes.img_divider} alt='divider'/>
-        <PurchaseItem/>
+        {
+          count !== 0 ? itemAndPage() : itemIsNone()
+        }
       </div>
     </div>
   );
